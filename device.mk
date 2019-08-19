a83e5b1c7797ce5d301dfd095aab8dda5c799394
miloxx <milosz.paszkowski@web.de>
2019-08-19 16:32:52 +0200

    separate kernel, dt image and fstab

--- device.mk
+++ device.mk
@@ -3,15 +3,21 @@ LOCAL_PATH := device/zte/kis3
 $(shell mkdir -p $(OUT_DIR)/obj/KERNEL_OBJ/usr)
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-				LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
-				LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += \
-			$(LOCAL_PATH)/kernel:kernel \
-			$(LOCAL_PATH)/dt.img:dt.img \
-			$(LOCAL_PATH)/recovery.fstab:root/recovery.fstab
+    $(LOCAL_KERNEL):kernel:kernel \
+
+# DT Image
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/dt.img:dt.img \
+
+# Kernel Ramdisk
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/recovery.fstab:root/recovery.fstab
 
 $(call inherit-product, build/target/product/full.mk)
 
